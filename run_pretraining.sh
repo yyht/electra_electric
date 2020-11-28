2914c277721e6a2804e6ac365294549cdccf8545
天逸 <albert.xht@alibaba-inc.com>
2020-11-28 10:25:19 +0800

    fix

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -1,5 +1,5 @@
 nohup python run_pretraining.py \
 	--data-dir gs://yyht_source/pretrain \
-	--data-file-list chinese_simplified_whole_sentence_v1/chinese_simplified_whole_sentence_file.txt \
+	--data-file-list chinese_simplified_whole_sentence_v2_32/chinese_simplified_whole_sentence_file.txt \
 	--model-name electric_base \
 	--hparams electric_params.json

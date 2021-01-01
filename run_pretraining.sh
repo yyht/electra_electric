a8557d9788745e11c71ae272f1d8a8188e0b83d4
天逸 <albert.xht@alibaba-inc.com>
2021-01-01 11:21:49 +0800

    fix nce-negative and add more monitoring values

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -1,5 +1,5 @@
 nohup python run_pretraining.py \
 	--data-dir gs://yyht_source/pretrain \
 	--data-file-list chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
-	--model-name electric_nce_base_pseudo \
+	--model-name electric_nce_base_not_tie_embed \
 	--hparams electric_nce_sent_params.json

64f248124d4a07fcb6ca90fe57aba8f5fd001385
天逸 <albert.xht@alibaba-inc.com>
2021-01-11 00:26:18 +0800

    fix

--- run_pretraining_tta.sh
+++ run_pretraining_tta.sh
@@ -8,7 +8,7 @@ nohup python run_pretraining_tta.py \
 	--train_batch_size 256 \
 	--learning_rate 1e-4 \
 	--num_train_steps 1000000 \
-	--num_warmup_steps 100000 \
+	--num_warmup_steps 10000 \
 	--save_checkpoints_steps 1000 \
 	--iterations_per_loop 1000 \
 	--use_tpu True \

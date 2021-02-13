4a89efdaa1b0a1f76a3c7f53ab4f6aa205dab8fe
天逸 <albert.xht@alibaba-inc.com>
2021-02-13 22:10:08 +0800

    add original bert and nystromformer

--- run_pretraining_bert_tiny.sh
+++ run_pretraining_bert_tiny.sh
@@ -1,4 +1,4 @@
-nohup python run_pretraining_nystromformer.py \
+nohup python run_pretraining_bert.py \
 	--bert_config_file ./config/bert_config_tiny.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
 	--output_dir gs://yyht_source/pretrain/models/bert_tiny_50g \

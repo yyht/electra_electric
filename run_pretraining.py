a5e064faa790568e2e385071d8411d37197c3ddf
天逸 <albert.xht@alibaba-inc.com>
2021-02-14 18:42:30 +0800

    fix

--- run_pretraining.py
+++ run_pretraining.py
@@ -1176,7 +1176,7 @@ def main():
     hparams = utils.load_json(args.hparams)
   else:
     hparams = json.loads(args.hparams)
-  tf.logging.set_verbosity(tf.logging.ERROR)
+  # tf.logging.set_verbosity(tf.logging.ERROR)
   train_or_eval(configure_pretraining.PretrainingConfig(
       args.model_name, args.data_dir, args.data_file_list, 
       args.generator_ckpt, args.discriminator_ckpt,

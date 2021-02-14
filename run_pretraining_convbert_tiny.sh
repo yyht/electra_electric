f5428fba124c402965a1c0436198e6e59e680a56
天逸 <albert.xht@alibaba-inc.com>
2021-02-14 22:03:37 +0800

    fix

--- run_pretraining_convbert_tiny.sh
+++ run_pretraining_convbert_tiny.sh
@@ -1,4 +1,4 @@
-nohup python run_pretraining_bert.py \
+nohup python run_pretraining_convbert.py \
 	--bert_config_file ./config/bert_config_tiny_official_conv.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
 	--output_dir gs://yyht_source/pretrain/models/convbert_tiny_50g \

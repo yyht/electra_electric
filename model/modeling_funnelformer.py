a8f4d4a456e2e4b9c45f50b4f536dd78585ec8ad
天逸 <albert.xht@alibaba-inc.com>
2021-02-19 17:58:07 +0800

    add dropout-reuse and fix electric-nce-bugs

--- model/modeling_funnelformer.py
+++ model/modeling_funnelformer.py
@@ -47,6 +47,21 @@ def get_initializer(net_config):
     raise ValueError("Initializer {} not supported".format(net_config.init))
   return initializer
 
+def gelu(x):
+  """Gaussian Error Linear Unit.
+
+  This is a smoother version of the RELU.
+  Original paper: https://arxiv.org/abs/1606.08415
+  Args:
+    x: float Tensor to perform activation.
+
+  Returns:
+    `x` with the GELU activation applied.
+  """
+  cdf = 0.5 * (1.0 + tf.tanh(
+      (np.sqrt(2 / np.pi) * (x + 0.044715 * tf.pow(x, 3)))))
+  return x * cdf
+
 def get_activation(activation_type):
   """Get the corresponding activation function from string."""
   if activation_type == "relu":

7fd1b3c6ed5c1cdd0f111b5ef03a2e8c83d675ca
天逸 <albert.xht@alibaba-inc.com>
2021-02-25 18:37:30 +0800

    add uncased english nce

--- run_pretraining_bert_funnelformer_xtiny.sh
+++ run_pretraining_bert_funnelformer_xtiny.sh
@@ -1,5 +1,5 @@
 nohup python run_pretraining_bert_funnelformer.py \
-	--bert_config_file ./config/bert_funnel_transformer_xtiny_2_chinese.json \
+	--bert_config_file ./config/bert_funnelformer_xtiny_2_chinese.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
 	--output_dir gs://yyht_source/pretrain/models/funnelformer_xtiny_2_50g \
 	--input_data_dir gs://yyht_source/pretrain \

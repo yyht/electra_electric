ba2b97e05901a77ac180129ee704fd526bfeffa0
天逸 <albert.xht@alibaba-inc.com>
2021-03-15 11:25:25 +0800

    add modeling_nystromformer with conv

--- run_pretraining_nystromformer.py
+++ run_pretraining_nystromformer.py
@@ -576,8 +576,10 @@ def main(_):
           num_shards=FLAGS.num_tpu_cores,
           per_host_input_for_training=is_per_host))
 
-  init_checkpoint = os.path.join(FLAGS.input_data_dir, FLAGS.init_checkpoint)
-
+  if FLAGS.init_checkpoint:
+    init_checkpoint = os.path.join(FLAGS.input_data_dir, FLAGS.init_checkpoint)
+  else:
+    init_checkpoint = None
   model_fn = model_fn_builder(
       bert_config=bert_config,
       init_checkpoint=init_checkpoint,

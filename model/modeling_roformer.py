a997d12e4f901720417407256bbb9b55c94d5314
天逸 <albert.xht@alibaba-inc.com>
2021-03-28 00:37:27 +0800

    add roformer

--- model/modeling_roformer.py
+++ model/modeling_roformer.py
@@ -881,13 +881,13 @@ def attention_layer(from_tensor,
     sin_pos = tf_utils.repeat(position_embeddings[:, :, :, ::2], repeats=2, axis=-1)
 
     # [1, F, 1, depth]
-    query_layer2 = tf.stack([-query_layer[:, :, :, 1::2], query_layer[:, :, :, 2::]], axis=-1)
+    query_layer2 = tf.stack([-query_layer[:, :, :, 1::2], query_layer[:, :, :, ::2]], axis=-1)
     
     # [B, N, F, depth] * [1, F, 1, depth]
     query_layer = query_layer * cos_pos + query_layer2 * sin_pos
 
     # [B, N, F, depth] * [1, F, 1, depth]
-    key_layer2 = tf.stack([-key_layer[:, :, :, 1::2], key_layer[:, :, :, 2::]], axis=-1)
+    key_layer2 = tf.stack([-key_layer[:, :, :, 1::2], key_layer[:, :, :, ::2]], axis=-1)
     key_layer = key_layer * cos_pos + key_layer2 * sin_pos
 
 

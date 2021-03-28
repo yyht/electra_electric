9890360ff07d49577c2073c3adf771cae9cfb69a
天逸 <htxu91@gmail.com>
2021-03-28 23:43:10 +0800

    set tfidf output shape

--- run_pretraining_bert_small_roformer.sh
+++ run_pretraining_bert_small_roformer.sh
@@ -1,7 +1,7 @@
 nohup python run_pretraining_bert_roformer.py \
-	--bert_config_file ./config/bert_config_tiny_roformer.json \
+	--bert_config_file ./config/bert_config_small_roformer.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_tiny_roformer_50g_official \
+	--output_dir gs://yyht_source/pretrain/models/bert_small_roformer_50g_official \
 	--input_data_dir gs://yyht_source/pretrain \
 	--max_seq_length 512 \
 	--do_train True \

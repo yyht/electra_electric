18bd884f004c0d7f0061239c2b627b07bccb42c5
天逸 <htxu91@gmail.com>
2021-07-15 23:23:03 +0800

    add repeated-ngram mining

--- run_pretraining_bert_repeated_ngram.sh
+++ run_pretraining_bert_repeated_ngram.sh
@@ -1,5 +1,5 @@
 nohup python3 ./run_pretraining_bert_repeated_ngram.py \
-	--bert_config_file ./config/bert_config.json \
+	--bert_config_file ./config/bert_config_all.json \
 	--data_path_dict ./config/chinese_data_config.json \
 	--output_dir gs://yyht_source/pretrain/models/roberta_base_repeated_ngram_sop_chinese \
 	--input_data_dir gs://yyht_source/pretrain \

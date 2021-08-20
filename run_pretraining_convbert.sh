0fd043387367a014ea2d61dd113336ffbe5d9b14
天逸 <htxu91@gmail.com>
2021-08-20 17:41:26 +0800

    add conv-rdrop-simcse

--- run_pretraining_convbert.sh
+++ run_pretraining_convbert.sh
@@ -1,7 +1,7 @@
 nohup python3 run_pretraining_convbert.py \
 	--bert_config_file ./config/bert_config_base_official_conv_datagrand.json \
 	--input_file datagrand21/datagrand_21_file_list.txt \
-	--output_dir gs://yyht_source/pretrain/models/datagrand_21_convbert_base \
+	--output_dir gs://yyht_source/pretrain/models/datagrand_21_convbert_base_simcse \
 	--input_data_dir gs://yyht_source/pretrain \
 	--max_seq_length 512 \
 	--init_checkpoint models/datagrand_21_convbert_base/model.ckpt-300000 \
@@ -13,7 +13,7 @@ nohup python3 run_pretraining_convbert.py \
 	--save_checkpoints_steps 10000 \
 	--iterations_per_loop 1000 \
 	--use_tpu True \
-	--tpu_name albert1 \
+	--tpu_name albert4 \
 	--num_tpu_cores 8 \
 	--eval_batch_size 256 \
 	--max_predictions_per_seq 78 \

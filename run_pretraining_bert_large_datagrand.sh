58856728240d836c6f911d5058852c7aa834386c
天逸 <htxu91@gmail.com>
2021-08-24 19:39:42 +0800

    add conv-rdrop-simcse

--- run_pretraining_bert_large_datagrand.sh
+++ run_pretraining_bert_large_datagrand.sh
@@ -1,19 +1,18 @@
 nohup python3 run_pretraining_bert.py \
 	--bert_config_file ./config/bert_config_datagrand_large.json \
 	--input_file datagrand21/datagrand_21_file_list.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_large_datagrand_rdrop \
+	--output_dir gs://yyht_source/pretrain/models/bert_large_datagrand \
 	--input_data_dir gs://yyht_source/pretrain \
-	--init_checkpoint models/bert_large_datagrand/model.ckpt-200000 \
 	--max_seq_length 512 \
 	--do_train True \
-	--train_batch_size 64 \
+	--train_batch_size 128 \
 	--learning_rate 1e-4 \
 	--num_train_steps 1000000 \
 	--num_warmup_steps 10000 \
 	--save_checkpoints_steps 100000 \
 	--iterations_per_loop 1000 \
 	--use_tpu True \
-	--tpu_name albert3 \
+	--tpu_name albert1 \
 	--num_tpu_cores 8 \
 	--eval_batch_size 256 \
 	--max_predictions_per_seq 78 \
@@ -21,7 +20,7 @@ nohup python3 run_pretraining_bert.py \
 	--lr_decay_power 1.0 \
 	--weight_decay_rate 0.01 \
 	--mask_strategy "span_mask" \
-	--model_fn_type "rdropout" \
+	--model_fn_type "normal" \
 	--kld_ratio 1.0 \
 	--if_simcse True \
 	--simcse_ratio 1.0

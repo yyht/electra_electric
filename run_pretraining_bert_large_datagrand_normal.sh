07e41fec500d3a7afb091d66a08442b3f271e96f
天逸 <htxu91@gmail.com>
2021-08-25 10:18:07 +0800

    fix

--- run_pretraining_bert_large_datagrand_normal.sh
+++ run_pretraining_bert_large_datagrand_normal.sh
@@ -1,12 +1,12 @@
 nohup python3 run_pretraining_bert.py \
 	--bert_config_file ./config/bert_config_datagrand_large.json \
 	--input_file datagrand21/datagrand_21_file_list.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_large_datagrand_rdrop_nosimcse \
+	--output_dir gs://yyht_source/pretrain/models/bert_large_datagrand \
 	--input_data_dir gs://yyht_source/pretrain \
 	--init_checkpoint models/chinese_roberta_wwm_large_ext_L-24_H-1024_A-16/bert_model.ckpt \
 	--max_seq_length 512 \
 	--do_train True \
-	--train_batch_size 64 \
+	--train_batch_size 128 \
 	--learning_rate 1e-4 \
 	--num_train_steps 1000000 \
 	--num_warmup_steps 10000 \
@@ -21,6 +21,6 @@ nohup python3 run_pretraining_bert.py \
 	--lr_decay_power 1.0 \
 	--weight_decay_rate 0.01 \
 	--mask_strategy "span_mask" \
-	--model_fn_type "rdropout" \
+	--model_fn_type "normal" \
 	--kld_ratio 1.0 \
 	--simcse_ratio 1.0

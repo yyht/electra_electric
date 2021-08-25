22f423181057d4deb3ad965459a65693eb7a3b71
天逸 <htxu91@gmail.com>
2021-08-25 10:33:48 +0800

    fix

--- run_pretraining_bert_small_small.sh
+++ run_pretraining_bert_small_small.sh
@@ -1,4 +1,4 @@
-nohup python run_pretraining_bert.py \
+nohup python3 run_pretraining_bert.py \
 	--bert_config_file ./config/bert_config_small_small.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
 	--output_dir gs://yyht_source/pretrain/models/bert_small_small_50g \

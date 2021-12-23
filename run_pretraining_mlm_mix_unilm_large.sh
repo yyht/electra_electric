d2b39e643fcaa0ff5677fb52c3151bf93b766734
天逸 <htxu91@gmail.com>
2021-12-23 17:14:57 +0800

    fix pretrained model

--- run_pretraining_mlm_mix_unilm_large.sh
+++ run_pretraining_mlm_mix_unilm_large.sh
@@ -1,9 +1,9 @@
 nohup python3 run_pretraining_mlm_mix_unilm.py \
 	--bert_config_file ./config/bert_config_ilm_large.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_large_50g_mix_ilm_v2 \
+	--output_dir gs://yyht_source/pretrain/models/bert_large_50g_mix_ilm_final \
 	--input_data_dir gs://yyht_source/pretrain \
-	--init_checkpoint models/chinese_roberta_wwm_large_ext_L-24_H-1024_A-16/bert_model.ckpt \
+	--init_checkpoint models/chinese_roberta_wwm_large_ext_L-24_H-1024_A-16_ilm/bert_model.ckpt \
 	--max_seq_length 512 \
 	--do_train True \
 	--train_batch_size 64 \

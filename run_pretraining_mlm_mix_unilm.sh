3129bf3c5a15678b6eb1ac5dab183a6c62e6183f
yyht <htxu91@gmail.com>
2021-12-26 14:30:35 +0000

    fix

--- run_pretraining_mlm_mix_unilm.sh
+++ run_pretraining_mlm_mix_unilm.sh
@@ -1,9 +1,8 @@
 nohup python3 run_pretraining_mlm_mix_unilm.py \
 	--bert_config_file ./config/bert_config_ilm.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_base_50g_mix_ilm_final \
+	--output_dir gs://yyht_source/pretrain/models/bert_base_50g_mix_ilm_raw \
 	--input_data_dir gs://yyht_source/pretrain \
-	--init_checkpoint models/chinese_L-12_H-768_A-12_ilm/bert_model.ckpt \
 	--max_seq_length 512 \
 	--do_train True \
 	--train_batch_size 128 \

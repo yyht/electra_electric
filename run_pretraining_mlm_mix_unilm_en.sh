8042167e83df7f044c904bd2a08c043e9ff4416b
天逸 <htxu91@gmail.com>
2021-12-26 17:56:24 +0800

    fix pretrained model

--- run_pretraining_mlm_mix_unilm_en.sh
+++ run_pretraining_mlm_mix_unilm_en.sh
@@ -1,9 +1,8 @@
 nohup python3 run_pretraining_mlm_mix_unilm.py \
 	--bert_config_file ./config/bert_config_ilm_uncased_en.json \
 	--input_file uncased_english_whole_sentence_v3_32/uncased_english_whole_sentence_file.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_base_50g_mix_ilm_uncased_en_final \
+	--output_dir gs://yyht_source/pretrain/models/bert_base_50g_mix_ilm_uncased_en_raw \
 	--input_data_dir gs://yyht_source/pretrain \
-	--init_checkpoint models/uncased_L-12_H-768_A-12_ilm/bert_model.ckpt \
 	--max_seq_length 512 \
 	--do_train True \
 	--train_batch_size 128 \

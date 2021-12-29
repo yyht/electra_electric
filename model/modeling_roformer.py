d865fe7f33e98b240721c94b33c2048341247dfd
天逸 <htxu91@gmail.com>
2021-12-29 13:54:52 +0800

    fix roformer-bugs

--- model/modeling_roformer.py
+++ model/modeling_roformer.py
@@ -534,35 +534,35 @@ def embedding_postprocessor(input_tensor,
     output += token_type_embeddings
 
   if use_position_embeddings:
-    assert_op = tf.assert_less_equal(seq_length, max_position_embeddings)
-    with tf.control_dependencies([assert_op]):
-      full_position_embeddings = tf.get_variable(
-          name=position_embedding_name,
-          shape=[max_position_embeddings, width],
-          initializer=create_initializer(initializer_range))
-      # Since the position embedding table is a learned variable, we create it
-      # using a (long) sequence length `max_position_embeddings`. The actual
-      # sequence length might be shorter than this, for faster training of
-      # tasks that do not have long sequences.
-      #
-      # So `full_position_embeddings` is effectively an embedding table
-      # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
-      # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
-      # perform a slice.
-      position_embeddings = tf.slice(full_position_embeddings, [0, 0],
-                                     [seq_length, -1])
-      num_dims = len(output.shape.as_list())
+    full_position_embeddings = tf.get_variable(
+        name=position_embedding_name,
+        shape=[max_position_embeddings, width],
+        initializer=create_initializer(initializer_range))
+    # Since the position embedding table is a learned variable, we create it
+    # using a (long) sequence length `max_position_embeddings`. The actual
+    # sequence length might be shorter than this, for faster training of
+    # tasks that do not have long sequences.
+    #
+    # So `full_position_embeddings` is effectively an embedding table
+    # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
+    # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
+    # perform a slice.
+    flat_pos_ids = tf.range(seq_length, dtype=tf.int32)
+    one_hot_pos_ids = tf.one_hot(flat_pos_ids, depth=max_position_embeddings)
+    position_embeddings = tf.matmul(one_hot_pos_ids, full_position_embeddings)
 
-      # Only the last two dimensions are relevant (`seq_length` and `width`), so
-      # we broadcast among the first dimensions, which is typically just
-      # the batch size.
-      position_broadcast_shape = []
-      for _ in range(num_dims - 2):
-        position_broadcast_shape.append(1)
-      position_broadcast_shape.extend([seq_length, width])
-      position_embeddings = tf.reshape(position_embeddings,
-                                       position_broadcast_shape)
-      output += position_embeddings
+    num_dims = len(output.shape.as_list())
+
+    # Only the last two dimensions are relevant (`seq_length` and `width`), so
+    # we broadcast among the first dimensions, which is typically just
+    # the batch size.
+    position_broadcast_shape = []
+    for _ in range(num_dims - 2):
+      position_broadcast_shape.append(1)
+    position_broadcast_shape.extend([seq_length, width])
+    position_embeddings = tf.reshape(position_embeddings,
+                                     position_broadcast_shape)
+    output += position_embeddings
 
   output = layer_norm_and_dropout(output, dropout_prob, dropout_name)
   return output

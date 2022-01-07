67239a9eaa8055a8c70d429859f9d835fc9423de
天逸 <htxu91@gmail.com>
2022-01-07 11:21:48 +0800

    fix

--- pretrain/span_mask_utils_ilm.py
+++ pretrain/span_mask_utils_ilm.py
@@ -537,6 +537,8 @@ def _decode_record(FLAGS, record, num_predict,
     ilm_segment_ids = tf.concat([ilm_segment_ids, ilm_pad], axis=0)
     ilm_input_mask = tf.cast(tf.not_equal(ilm_input, 0), dtype=tf.int32)
 
+    ilm_relative_position = tf.cast(tf.cumsum(ilm_segment_ids), dtype=tf.int32) * ilm_input_mask
+
   elif FLAGS.ilm_v2:
 
     tf.logging.info("** apply different placeholder [unused] **")
@@ -571,6 +573,7 @@ def _decode_record(FLAGS, record, num_predict,
     ilm_segment_ids = tf.concat([ilm_segment_ids, ilm_pad], axis=0)
     ilm_input_mask = tf.cast(tf.not_equal(ilm_input, 0), dtype=tf.int32)
 
+    ilm_relative_position = tf.cast(tf.cumsum(ilm_segment_ids), dtype=tf.int32) * ilm_input_mask
   
   if FLAGS.ilm_v2 or FLAGS.ilm_v1:
     tgt_shape = inputs.shape.as_list()
@@ -578,10 +581,12 @@ def _decode_record(FLAGS, record, num_predict,
     ilm_input.set_shape(tgt_shape)
     ilm_segment_ids.set_shape(tgt_shape)
     ilm_input_mask.set_shape(tgt_shape)
+    ilm_relative_position.set_shape(tgt_shape)
     
     example['ilm_input'] = ilm_input
     example['ilm_segment_ids'] = ilm_segment_ids
     example['ilm_input_mask'] = ilm_input_mask
+    example['ilm_relative_position'] = ilm_relative_position
 
   # type cast for example
   convert_example(example, use_bfloat16)
--- run_pretraining_bert_base_relative_t5_sinusodial_fixed.sh
+++ run_pretraining_bert_base_relative_t5_sinusodial_fixed.sh
@@ -1,18 +1,18 @@
 nohup python3 run_pretraining_bert_relative_sinusoidal.py \
-	--bert_config_file ./config/bert_config_small_relative_t5_sinusoidal.json \
+	--bert_config_file ./config/bert_config_base_relative_t5_sinusoidal_fixed.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_small_relative_t5_sinusoidal_50g_official \
+	--output_dir gs://yyht_source/pretrain/models/bert_base_relative_t5_sinusoidal_fixed_50g_final \
 	--input_data_dir gs://yyht_source/pretrain \
 	--max_seq_length 512 \
 	--do_train True \
-	--train_batch_size 512 \
+	--train_batch_size 256 \
 	--learning_rate 1e-4 \
 	--num_train_steps 1000000 \
 	--num_warmup_steps 10000 \
 	--save_checkpoints_steps 1000 \
 	--iterations_per_loop 1000 \
 	--use_tpu True \
-	--tpu_name albert4 \
+	--tpu_name albert2 \
 	--num_tpu_cores 8 \
 	--eval_batch_size 256 \
 	--max_predictions_per_seq 78 \

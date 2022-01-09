cf201b24cb2a4fa05c0b2839e84c2d2f061352bf
天逸 <htxu91@gmail.com>
2022-01-09 22:57:35 +0800

    fix

--- run_pretraining_bert_base_relative_t5_sinusodial_en.sh
+++ run_pretraining_bert_base_relative_t5_sinusodial_en.sh
@@ -1,5 +1,5 @@
 nohup python3 run_pretraining_bert_relative_sinusoidal.py \
-	--bert_config_file ./config/bert_config_base_relative_t5_sinusoidal.json \
+	--bert_config_file ./config/bert_config_base_relative_t5_sinusoidal_en.json \
 	--input_file uncased_english_whole_sentence_v3_32/uncased_english_whole_sentence_file.txt \
 	--output_dir gs://yyht_source/pretrain/models/bert_base_relative_t5_sinusoidal_50g_en_official \
 	--input_data_dir gs://yyht_source/pretrain \

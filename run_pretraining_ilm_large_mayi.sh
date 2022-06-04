5e46ea37dd4ae733189a41e174198e46a542cc78
天逸 <albert.xht@alibaba-inc.com>
2022-06-04 14:27:46 +0800

    only ilm

--- run_pretraining_ilm_large_mayi.sh
+++ run_pretraining_ilm_large_mayi.sh
@@ -1,12 +1,12 @@
 nohup python3 run_pretraining_mlm_mix_unilm.py \
 	--bert_config_file ./config/bert_config_ilm_large.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
-	--output_dir gs://yyht_source/pretrain/models/bert_large_50g_mix_ilm_final_mayi \
+	--output_dir gs://yyht_source/pretrain/models/bert_large_50g_ilm_final_mayi \
 	--input_data_dir gs://yyht_source/pretrain \
 	--init_checkpoint models/chinese_L-24_H-1024_A-16-mayi_v1_ilm/bert_model.ckpt.ckpt \
 	--max_seq_length 512 \
 	--do_train True \
-	--train_batch_size 64 \
+	--train_batch_size 128 \
 	--learning_rate 1e-4 \
 	--num_train_steps 1000000 \
 	--num_warmup_steps 10000 \

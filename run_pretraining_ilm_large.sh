1ad417f94409ff39f856ad698fcdeb8af494fe11
天逸 <albert.xht@alibaba-inc.com>
2022-06-04 14:50:17 +0800

    only ilm

--- run_pretraining_ilm_large.sh
+++ run_pretraining_ilm_large.sh
@@ -6,7 +6,7 @@ nohup python3 run_pretraining_mlm_mix_unilm.py \
 	--init_checkpoint models/chinese_roberta_wwm_large_ext_L-24_H-1024_A-16_ilm/bert_model.ckpt.ckpt \
 	--max_seq_length 512 \
 	--do_train True \
-	--train_batch_size 128 \
+	--train_batch_size 64 \
 	--learning_rate 1e-4 \
 	--num_train_steps 1000000 \
 	--num_warmup_steps 10000 \

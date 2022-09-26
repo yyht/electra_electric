07f0c6c42935c04346f93ed7275ba48c4d2c55b4
天逸 <albert.xht@alibaba-inc.com>
2022-09-26 16:46:29 +0800

    add roformer-ilm

--- run_pretraining_ilm_roformer.sh
+++ run_pretraining_ilm_roformer.sh
@@ -1,9 +1,9 @@
 nohup python3 run_pretraining_ilm.py \
 	--bert_config_file ./config/bert_config_ilm_roformer.json \
 	--input_file chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
-	--output_dir gs://yyht_source/pretrain/models/roformer_base_50g_ilm_final \
+	--output_dir gs://yyht_source/pretrain/models/bert_base_roformer_50g_ilm_latest \
 	--input_data_dir gs://yyht_source/pretrain \
-	--init_checkpoint models/chinese_L-12_H-768_A-12_ilm_v1/bert_model.ckpt \
+	--init_checkpoint models/bert_base_roformer_50g_latest/model.ckpt-1000000 \
 	--max_seq_length 512 \
 	--do_train True \
 	--train_batch_size 128 \

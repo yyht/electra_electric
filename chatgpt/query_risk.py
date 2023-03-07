1f3278a0aaf3360f944e58826bd63fe5a9c71515
天逸 <albert.xht@alibaba-inc.com>
2023-03-07 08:20:53 +0800

    fix

--- chatgpt/query_risk.py
+++ chatgpt/query_risk.py
@@ -3,7 +3,7 @@ import openai
 import json
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = "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"
+# openai.api_key = "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"
 
 template = u"""
 选择题，请运用你的知识选择正确的答案和原因，正确的答案可能包含多个选项。\n问题:{}。这个问题是否包含 A:冒犯,B:辱骂,C:色情,D:危险行为,E:违法/犯罪行为,F:自我伤害,G:暴力,H:政治/宗教/历史,I:偏见,J:涉及公司/人物声誉,K:赌博,L:负面价值观,M:个人隐私信息\n 并说明原因
@@ -13,11 +13,21 @@ from tqdm import tqdm
 import numpy as np
 import time
 
-with open('/home/htxu91/rlhf/black_final.json.detail', 'w') as fwobj:
+key = [
+    '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',
+    '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',
+    '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',
+    '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'
+]
+
+with open('/home/htxu91/rlhf/black_final.json.detail.latest', 'w') as fwobj:
     with open('/home/htxu91/rlhf/black_final.json', 'r') as frobj:
         for idx, line in tqdm(enumerate(frobj)):
             content = json.loads(line.strip())
             sent = content['text']
+
+            random_key = np.random.choice(key)
+            openai.api_key = random_key
 
             if idx <= 844:
                 continue
@@ -34,7 +44,7 @@ with open('/home/htxu91/rlhf/black_final.json.detail', 'w') as fwobj:
                     #   message = 'invalid'
                 #   continue
 
-            time.sleep(10)
+            time.sleep(30)
             
             if np.mod(idx, 1000) == 0:
                 print(sent, '===', message, '====model====', 'gpt-3.5-turbo')
